547f1f035546cb8d5433871cf788ed20d5b83102
Renlor <Renlor@liddev.com>
2015-09-07 23:43:21 -0600

    Initial commit for homework cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'homework'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures homework'
+long_description 'Installs/Configures homework'
+version          '0.1.0'
