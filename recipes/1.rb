8eced4d627cc5ce5051048f9e875936c629a11aa
unknown <renlor@liddev.com>
2015-09-13 19:05:29 -0600

    Homework one added.

--- recipes/1.rb
+++ recipes/1.rb
@@ -5,3 +5,11 @@ directory '/home/ensign/assignments' do
   group     'ensign'
   mode      00755
 end
+
+remote_directory '/home/ensign/assignments/hw1' do
+  source 'hw1'
+  owner 'ensign'
+  group 'ensign'
+  mode '0755'
+  action :create_if_missing
+end
