a0368e94ba7b0dddae9fe16fe24e7a8d62ab4e17
unknown <renlor@liddev.com>
2015-09-14 00:25:28 -0600

    Fixed path issues.

--- recipes/1.rb
+++ recipes/1.rb
@@ -1,13 +1,12 @@
 
-directory '/home/ensign/assignments/hw1' do
+directory '/home/ensign/assignments' do
   recursive true
   owner     'ensign'
   group     'ensign'
   mode      00755
 end
 
-
-remote_directory '/home/ensign/assignments/hw1/lib' do
+remote_directory '/home/ensign/assignments/hw1' do
   source      'hw1'
   recursive   true
   owner       'ensign'
@@ -15,49 +14,4 @@ remote_directory '/home/ensign/assignments/hw1/lib' do
   mode        00755
   action      :create
   overwrite   false
-end
-
-
-remote_directory '/home/ensign/assignments/hw1/spec' do
-  source      'hw1/spec'
-  recursive   true
-  owner       'ensign'
-  group       'ensign'
-  mode        00755
-  action      :create
-  overwrite   false
-end
-
-cookbook_file '/home/ensign/assignments/hw1/Guardfile' do
- source      'hw1/Guardfile'
- owner       'ensign'
- group       'ensign'
- mode        00755
- action      :create_if_missing
-end
-
-
-cookbook_file '/home/ensign/assignments/hw1/Gemfile' do
-  source      'hw1/Gemfile'
-  owner       'ensign'
-  group       'ensign'
-  mode        00755
-  action      :create_if_missing
-end
-
-
-cookbook_file '/home/ensign/assignments/hw1/Gemfile.lock' do
-  source      'hw1/Gemfile.lock'
-  owner       'ensign'
-  group       'ensign'
-  mode        00755
-  action      :create_if_missing
-end
-
-cookbook_file '/home/ensign/assignments/hw1/README.md' do
-  source      'hw1/README.md'
-  owner       'ensign'
-  group       'ensign'
-  mode        00755
-  action      :create_if_missing
 end
